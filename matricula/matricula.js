5fc3aefb9a70824713466535e9f767dcfe8a2530
Elaine matos <nany.sara@hotmail.com>
2021-12-19 04:29:43 -0300

    Projeto plataforma de educação, desafio dowhile

---
+++ matricula/matricula.js
@@ -0,0 +1,24 @@
+
+function logando() {
+    if (document.querySelector(".logar").active == true) {
+        window.location.href = "matricula.html"
+
+    } else {
+        window.location.href = "matricula2.html"
+
+    } 
+
+
+}
+function cadastrando() {
+    if (document.querySelector(".cadastrar").active == true) {
+        window.location.href = "matricula2.html"
+
+    } else {
+        window.location.href = "matricula.html"
+
+    }
+    
+
+
+}
